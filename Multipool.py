50bd2fbbaf384d71ab06fa8df10ff5c0c92a4cdd
sanzeed <sanzeed@mit.edu>
2019-07-12 16:53:37 -0400

    Created Multipool class to accommodate non-daemon pool processes

---
+++ Multipool.py
@@ -0,0 +1,14 @@
+# credit: https://stackoverflow.com/questions/6974695/python-process-pool-non-daemonic
+
+import multiprocessing
+
+
+class NonDaemonProcess(multiprocessing.Process):
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+class Multipool(multiprocessing.pool.Pool):
+    Process = NonDaemonProcess
