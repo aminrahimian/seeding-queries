de770859ca94e33b9d9bbab2e216006355305f3d
sanzeed <sanzeed@mit.edu>
2020-01-30 22:05:05 -0500

    Changed script to get data in csv file

--- get_data_in_csv.py
+++ get_data_in_csv.py
@@ -5,7 +5,7 @@ import pickle
 import numpy as np
 
 root = './data/fb100-data/pickled_samples/spreading_pickled_samples/'
-costs = [0, 1, 2, 3, 4, 5, 7, 8, 10, 13, 16, 20, 24, 30, 37, 46, 57, 71, 88, 109, 134, 166, 206, 255, 315, 390]
+costs = [0, 1, 2, 3, 4, 5, 7, 8, 10, 13, 16, 20, 24, 30, 37, 46, 57]
 
 
 def get_edge_query_data():
@@ -27,31 +27,21 @@ def get_edge_query_data():
                                         + network_group + network_id
                                         + '_T_' + str(T)
                                         + model_id + '.pkl', 'rb'))
-                if T == 0:
-                    costs_with_leaves = [(0, 0) for i in range(50)]
-                    costs_without_leaves = [(0, 0) for i in range(50)]
-                else:
-                    costs_with_leaves = pickle.load(open(root + 'k_' + str(k)
-                                            + '/edge_query'
-                                            + '/' + network_id
-                                            + '/cost_samples_with_leaf_'
-                                            + network_group + network_id
-                                            + '_T_' + str(T)
-                                            + model_id + '.pkl', 'rb'))
-                    costs_without_leaves = pickle.load(open(root + 'k_' + str(k)
-                                                            + '/edge_query'
-                                                            + '/' + network_id
-                                                            + '/cost_samples_without_leaf_'
-                                                            + network_group + network_id
-                                                            + '_T_' + str(T)
-                                                            + model_id + '.pkl', 'rb'))
+                
+                costs = pickle.load(open(root + 'k_' + str(k)
+                                        + '/edge_query'
+                                        + '/' + network_id
+                                        + '/cost_samples_'
+                                        + network_group + network_id
+                                        + '_T_' + str(T)
+                                        + model_id + '.pkl', 'rb'))
 
-                for seed_set_id in range(50):
-                    for sample_id in range(500):
-                        writer.writerow([k, T, seed_set_id, sample_id, 
-                                        spreads[500 * seed_set_id + sample_id],
-                                        costs_with_leaves[seed_set_id][0], costs_without_leaves[seed_set_id][0],
-                                        costs_with_leaves[seed_set_id][1], costs_without_leaves[seed_set_id][1]])
+            for seed_set_id in range(50):
+                for sample_id in range(500):
+                    writer.writerow([k, T, seed_set_id, sample_id, 
+                                    spreads[500 * seed_set_id + sample_id],
+                                    costs[seed_set_id][0], costs[seed_set_id][1],
+                                    costs[seed_set_id][2], costs[seed_set_id][3]])
 
 
 if __name__ == '__main__':
