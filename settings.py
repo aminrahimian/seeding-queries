52ecb7da81f662e7bdf57e3ac2c88a681148e66c
Amin Rahimian <rahimian@mit.edu>
2020-02-08 17:10:49 -0500

    removed unused settings

--- settings.py
+++ settings.py
@@ -11,17 +11,6 @@ import errno
 import networkx as NX
 import re
 
-susceptible = 0.0
-infected = 1.0
-
-active = 0.5
-inactive = -0.5
-
-SIMPLE = 0
-COMPLEX = 1
-
-SENTINEL = object()
-
 
 def get_n_smallest_key_values(dictionary, n):
     smallest_entries = sorted(
@@ -45,17 +34,10 @@ def natural_keys(text):
 # real world networks simulation settings:
 network_group = 'fb100_edgelist_'
 # 'banerjee_combined_edgelist_'
-#'chami_union_edgelist_'
+# 'cai_edgelist_'
 # 'chami_union_edgelist_'
-# 'fb100_edgelist_'
-# 'cai_edgelist_'
 # 'chami_advice_edgelist_'
-# 'banerjee_combined_edgelist_'
-# 'cai_edgelist_' #'fb100_edgelist_'
-# 'banerjee_combined_edgelist_'
 # 'chami_friendship_edgelist_'
-# 'chami_advice_edgelist_'
-# 'cai_edgelist_'
 
 if network_group == 'cai_edgelist_':
 
@@ -102,44 +84,12 @@ elif network_group == 'fb100_edgelist_':
 
 edgelist_directory_address = root_data_address + 'edgelists/'
 
-output_directory_address = root_data_address + 'output/'
-
 pickled_samples_directory_address = root_data_address + 'pickled_samples/'
-
-properties_pickled_samples_directory_address = pickled_samples_directory_address + 'properties_pickled_samples/'
 
 spreading_pickled_samples_directory_address = pickled_samples_directory_address + 'spreading_pickled_samples/'
 
-networks_pickled_samples_directory_address = pickled_samples_directory_address + 'networks_pickled_samples/'
-
-use_separate_address_for_pickled_networks = False  # pickled_networks take a lot of space.
-# Some may need to put them else where with a lot of space away from other pickled samples.
-
-separate_address_for_pickled_networks = \
-    '/home/rahimian/seeding_queries/data/pickled_networks/'
-# '/home/rahimian/seeding_queries/data/pickled_networks/'
-#  '/home/amin/Desktop/pickled_networks/'
-
-if use_separate_address_for_pickled_networks:
-    networks_pickled_samples_directory_address = \
-        separate_address_for_pickled_networks
-
-try:
-    os.makedirs(output_directory_address)
-except OSError as e:
-    pass
-#    if e.errno != errno.EEXIST:
-#        raise
-
 try:
     os.makedirs(pickled_samples_directory_address)
-except OSError as e:
-    pass
-#    if e.errno != errno.EEXIST:
-#        raise
-
-try:
-    os.makedirs(properties_pickled_samples_directory_address)
 except OSError as e:
     pass
 #    if e.errno != errno.EEXIST:
@@ -152,15 +102,7 @@ except OSError as e:
 #    if e.errno != errno.EEXIST:
 #        raise
 
-try:
-    os.makedirs(networks_pickled_samples_directory_address)
-except OSError as e:
-    pass
-#    if e.errno != errno.EEXIST:
-#        raise
-
 #  different spreading models:
-
 model_id = '_vanilla IC_'
 
 if model_id == '_vanilla IC_':
@@ -251,8 +193,6 @@ try:
 
         print(network_id_list)
 
-
-
 except NameError:
 
     print('could not check for availability of samples or take smaller ones')
@@ -282,86 +222,12 @@ if 'SLURM_ARRAY_TASK_ID' in os.environ:
 #     batch_id_list = [BATCH_ID]
 #     print('BATCH_ID_LIST:', BATCH_ID)
 
-# theory simulations settings:
-
-simulation_type = 'ck_union_ER_vs_size'
-#  'c1_c2_interpolation'
-#  'ck_union_ER_vs_size'
-#  'ck_union_ER_vs_k'
-#  'c1_c2_interpolation'
-#  'c1_union_ER'
-#  'c1_c2_interpolation_SimpleOnlyAlongC1'
-
-root_theory_simulations_address = './data/theory-simulations/'
-
-try:
-    os.makedirs(root_theory_simulations_address)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
-
-theory_simulation_output_address = root_theory_simulations_address + simulation_type + '/output/'
-
-theory_simulation_pickle_address = root_theory_simulations_address + simulation_type + '/pickled_samples/'
-
-try:
-    os.makedirs(theory_simulation_output_address)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
-
-try:
-    os.makedirs(theory_simulation_pickle_address)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
-
 # commonly used settings:
 #
 # for computations:
 do_computations = True
 do_multiprocessing = False
 save_computations = True
-load_computations = False
-do_plots = False
-save_plots = False
-show_plots = False
-data_dump = False
-simulator_mode = False
-#
-# for plotting:
-# do_computations = False
-# do_multiprocessing = False
-# save_computations = False
-# load_computations = True
-# do_plots = True
-# save_plots = True
-# show_plots = False
-# data_dump = False
-# simulator_mode = False
-#
-# # # for data_dump:
-# do_computations = False
-# do_multiprocessing = False
-# save_computations = False
-# load_computations = True
-# do_plots = False
-# save_plots = False
-# show_plots = False
-# data_dump = True
-# simulator_mode = False
-
-# for simulator:
-# do_computations = True
-# do_multiprocessing = False
-# save_computations = False
-# load_computations = False
-# # simulator uses a different mathplotlib setting for plotting
-# do_plots = False
-# save_plots = False
-# show_plots = False
-# data_dump = False
-# simulator_mode = True
 
 #  check that different modes are set consistently
 
@@ -369,43 +235,6 @@ assert (not save_computations) or do_computations, "do_computations should be tr
 
 assert (not do_multiprocessing) or do_computations, "do_computations should be true to do_multiprocessing"
 
-assert (not (save_plots or show_plots)) or do_plots, "do_plots should be true to save_plots or show_plots"
-
-assert (not do_plots) or save_plots or show_plots, "when do_plots should either save_plots or show_plots"
-
-assert not (load_computations and save_computations), "cannot both save_computations and load_computations"
-
-assert not (load_computations and do_computations), "cannot both do_computations and load_computations"
-
-assert not (show_plots and save_plots), "you can either save plots or show plots not both"
-
-assert not (data_dump and (save_plots or show_plots)), "you cannot do any plots in data_dump mode"
-
-assert not (data_dump and (save_computations or do_computations)), "you cannot do any computations in data_dump mode"
-
-assert (not data_dump) or load_computations, "load_computations should be true to data_dump"
-
-assert not (simulator_mode and do_plots), "simulator_mode and do_plots use different " \
-                                          "(conflicting) matplotlib settings, and " \
-                                          "cannot be both true"
-
-
-
-# import the required packages for different modes:
-
-if data_dump:
-    import csv
-    import pandas as pd
-
-if do_plots:
-    import matplotlib.pyplot as plt
-    plt.rc('text', usetex=True)
-    plt.rc('font', ** {'family': 'sans-serif', 'sans-serif':['Helvetica']})
-    plt.rcParams.update({'font.size': 15})
-    from decimal import Decimal
-    FOURPLACES = Decimal(10) ** -4
-    TWOPLACES = Decimal(10) ** -2
-    ERROR_BAR_TYPE = 'std'
 
 if do_multiprocessing:
     import multiprocessing
@@ -415,14 +244,3 @@ if do_multiprocessing:
     else:
         number_CPU = 20
 
-
-def combine(list_of_names,output_name):
-    '''Useful for combining pickle files from cluster computations.'''
-    original = pickle.load(open('./data/' + list_of_names[0] + '.pkl', 'rb'))
-    for ii in range(1, len(list_of_names)):
-        additional = pickle.load(open('./data/' + list_of_names[ii] + '.pkl', 'rb'))
-        print(original)
-        print(additional)
-        original = np.concatenate((original,additional),axis=1)
-        print(original)
-    pickle.dump(original, open('./data/'+output_name+'.pkl', 'wb'))
