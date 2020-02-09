09e0f4cdbdcfd7bb3f59430955f0b84c68ca798c
Amin Rahimian <rahimian@mit.edu>
2020-02-08 19:19:34 -0500

    added node and edge cost analysis for spread queries

--- simulation_results.R
+++ simulation_results.R
@@ -206,7 +206,10 @@ sp = sp %>%
   mutate(
     spread_size_percent = 100 * spread_size / penn_max_nodes,
     revenue = 0.1 * spread_size,
-    cost = 10*k + num_of_spread_query,
+    cost = 10*k + query_cost,
+    node_cost_wol_percent = 100 * node_cost_wol / penn_max_nodes,
+    node_cost_percent = 100 * node_cost / penn_max_nodes,
+    edge_cost_percent = 100 * edge_cost / penn_max_edges
   ) 
 
 sp = sp %>%
@@ -216,28 +219,34 @@ sp = sp %>%
 
 # getting the expected spread sizes and profits
 
-sps = sp %>%
-  group_by(k,num_of_spread_query, seed_set_id) %>%
+sps = sp %>% filter(query_cost<180) %>%
+  group_by(k,query_cost, seed_set_id) %>%
   summarise(
     n = n(),
     spread_size_expected = mean(spread_size_percent),
-    profit_expected = mean(profit)
+    profit_expected = mean(profit),
+    node_cost_percent = node_cost_percent[1],
+    edge_cost_percent = edge_cost_percent[1]
   )
 
 spss = sps %>%
-  group_by(k,num_of_spread_query) %>%
+  group_by(k,query_cost) %>%
   summarise(
     n_seed_set_ids = n(),
     spread_size_mean_se = sd(spread_size_expected) / sqrt(n()),
     spread_size_mean = mean(spread_size_expected),
     profit_mean_se = sd(profit_expected) / sqrt(n()),
-    profit_mean = mean(profit_expected)
+    profit_mean = mean(profit_expected),
+    node_cost_mean = mean(node_cost_percent),
+    node_cost_mean_se = sd(node_cost_percent) / sqrt(n()),
+    edge_cost_mean = mean(edge_cost_percent),
+    edge_cost_mean_se = sd(edge_cost_percent) / sqrt(n())
   )
 
 # show distribution of spread sizes
 ggplot(
-  aes(x = num_of_spread_query, y = spread_size_percent,
-      color = factor(k), group = num_of_spread_query),
+  aes(x = query_cost, y = spread_size_percent,
+      color = factor(k), group = query_cost),
   data = sp
 ) +
   facet_wrap( ~ k, labeller = label_both) +
@@ -249,8 +258,8 @@ ggplot(
   )
 
 ggplot(
-  aes(x = factor(num_of_spread_query), y = spread_size_percent,
-      color = factor(k), group = paste(num_of_spread_query, sprintf("%02d", k))),
+  aes(x = factor(query_cost), y = spread_size_percent,
+      color = factor(k), group = paste(query_cost, sprintf("%02d", k))),
   data = sp
 ) +
   geom_boxplot(
@@ -280,7 +289,7 @@ ggsave("figures/spread_queries_spread_size_boxplot_num_of_spread_query.pdf", wid
 
 ggplot(
   aes(
-    x = num_of_spread_query, 
+    x = query_cost, 
     y = spread_size_mean,
     ymin = spread_size_mean - qt(.975, df = n_seed_set_ids) * spread_size_mean_se,
     ymax = spread_size_mean + qt(.975, df = n_seed_set_ids) * spread_size_mean_se,
@@ -312,7 +321,7 @@ ggsave("figures/spread_queries_spread_size_by_query_cost.pdf", width = 4.5, heig
 
 ggplot(
   aes(
-    x = num_of_spread_query, 
+    x = query_cost, 
     y = profit_mean,
     ymin = profit_mean - qt(.975, df = n_seed_set_ids) * profit_mean_se,
     ymax = profit_mean + qt(.975, df = n_seed_set_ids) * profit_mean_se,
@@ -338,4 +347,36 @@ ggplot(
   theme(legend.position = c(0.9, 0.25))
 
 ggsave("figures/spread_queries_profit_by_query_cost.pdf", width = 4.5, height = 3.5)
+
+query_cost_to_node_cost_trans_func = function(x) approx(spss$query_cost, spss$node_cost_mean, xout = x)$y
+spread_query_edge_to_node_cost_trans_func = function(x) approx(spss$edge_cost_mean, spss$node_cost_mean, xout = x)$y
+
+ggplot(
+  aes(
+    x = edge_cost_mean, 
+    y = spread_size_mean,
+    ymin = spread_size_mean - qt(.975, df = n_seed_set_ids) * spread_size_mean_se,
+    ymax = spread_size_mean + qt(.975, df = n_seed_set_ids) * spread_size_mean_se,
+    color = factor(k), shape = factor(k), group = k
+  ),
+  data = spss
+) +
+  geom_line() +
+  geom_pointrange(size = .4) +
+  scale_color_manual(
+    name = "k", values = k_colors,
+    guide = guide_legend(reverse = TRUE)
+  ) +
+  scale_shape_discrete(name = "k", guide = guide_legend(reverse = TRUE)) +
+  scale_y_continuous(name = "spread size (mean %)") +
+  scale_x_continuous(
+    name = "edges queried (mean %)",
+    sec.axis = sec_axis(
+      ~ spread_query_edge_to_node_cost_trans_func(.),
+      name = "nodes queried (mean %)"
+    )
+  ) +
+  theme(legend.position = c(0.9, 0.25))
+
+ggsave("figures/spread_queries_spread_size_by_edges_and_nodes_queried.pdf", width = 4.5, height = 3.5)
 
